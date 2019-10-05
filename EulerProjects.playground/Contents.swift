4baa6a04d92078461b0646fea0212e799534040a
Dilgir Siddiqui <dsiddiqui@apple.com>
2019-10-05 00:02:52 -0700

    Find Prime numbers upto given number

--- EulerProjects.playground/Contents.swift
+++ EulerProjects.playground/Contents.swift
@@ -1,1 +1,7 @@
+import Cocoa
 // Test Euler's Projects
+
+// Find Prime Numbers upto number n
+
+var primeNumbers = PrimeNumbers()
+print("Prime Numbers: \(String(describing: primeNumbers.findPrimeNumbers(forInput: 50)))")
--- EulerProjects.playground/Sources/PrimeNumbers.swift
+++ EulerProjects.playground/Sources/PrimeNumbers.swift
@@ -1,4 +1,26 @@
 import Foundation
 
-class PrimeNumbers {
+public class PrimeNumbers {
+    
+    public init() {}
+    
+    public func findPrimeNumbers(forInput input: Int) -> [Int]? {
+        var result = [Int]()
+        var isPrime = false
+        
+        for i in 2...input {
+            isPrime = true
+            for j in 2..<i {
+                if i%j == 0 {
+                    isPrime = false
+                    break
+                }
+            }
+            if isPrime {
+                result.append(i)
+            }
+        }
+        
+        return result
+    }
 }
